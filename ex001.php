899c8ce798bc88df6def8cedb190daf680d4a66d
Gabriel Felipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-03-15 20:25:14 -0300

    Create ex001.php

---
+++ ex001.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		
+		<title>PHP test</title>
+	</head>
+	<body>
+		
+		<h1>
+			<?php
+				echo "Olá mundo \u{1F30E}"
+			?>
+		</h1>
+		
+		<p>Vamos nos livrar da maldição</p>
+	</body>
+</html>
