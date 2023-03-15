6cd2d754a32bb23fdd546e43c9534643a475c5da
Gabriel Felipe <gabrielfelipeassuncaodesouza@gmail.com>
2023-03-15 20:33:02 -0300

    Update ex001.php

--- ex001.php
+++ ex001.php
@@ -1,17 +1,16 @@
-<html>
+<!DOCTYPE html>
+<html lang="pt-br">
 	<head>
 		<meta charset="UTF-8">
-		
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>PHP test</title>
 	</head>
 	<body>
-		
 		<h1>
 			<?php
 				echo "Olá mundo \u{1F30E}"
 			?>
 		</h1>
-		
 		<p>Vamos nos livrar da maldição</p>
 	</body>
 </html>
